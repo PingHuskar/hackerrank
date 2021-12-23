199d3b4900dc43803fe4e40aec0d8134f3ba3a3d
Chadin Chaipornpisuth <61967318+PingHuskar@users.noreply.github.com>
2021-12-23 23:06:33 +0700

    python 3 swapcase method
    https://www.geeksforgeeks.org/python-string-swapcase

--- python/py-strings/swap-case.py
+++ python/py-strings/swap-case.py
@@ -4,14 +4,8 @@
 # https://www.hackerrank.com/challenges/swap-case/problem
 #
 
-def swap(c):
-    if c.islower():
-        return c.upper()
-    else:
-        return c.lower()
-
 def swap_case(s):
-    return ''.join([swap(c) for c in s])
+    return s.swapcase()
 
 # (skeliton_tail) ----------------------------------------------------------------------
 if __name__ == '__main__':
